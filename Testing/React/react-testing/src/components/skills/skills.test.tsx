0f55e939d08d7749ac266453abe5aba835bf6d54
Yelihi <Yelihi19@gmail.com>
2023-06-11 21:06:00 +0900

    Add: TIL react-testing

--- Testing/React/react-testing/src/components/skills/skills.test.tsx
+++ Testing/React/react-testing/src/components/skills/skills.test.tsx
@@ -24,6 +24,7 @@ describe("Skills", () => {
     expect(loginButtonElement).toBeInTheDocument();
   });
 
+  // 특이점은 버튼을 눌러야 DOM 에 추가되는 startButton 은 get 으로 할 수 없다. DOM 에 없기 때문이다. 이때 사용하는것이 query 이다
   it("Start learning button is not rendered", () => {
     render(<Skills skills={skills} />);
     const startLearnningButton = screen.queryByRole("button", {
