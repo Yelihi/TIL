cc1831c248c6113de56ab8edaf47479ecd2f8ff5
Yelihi <Yelihi19@gmail.com>
2023-06-17 17:28:20 +0900

    Add: Testing msw

--- Testing/React/react-testing/src/components/users/users.test.tsx
+++ Testing/React/react-testing/src/components/users/users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./users";
+import { rest } from "msw";
+import { server } from "../../mocks/server";
 
 describe("Users", () => {
   it("render correctly", () => {
@@ -12,5 +14,16 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  it("renders error", async () => {
+    server.use(
+      rest.get("http://jsonplaceholder.typicode.com/users", (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    render(<Users />);
+    const errorMessage = await screen.findByText("Error fetching users");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
