ecdf5955b1ff25b60957104f6daea2fd0a8ea13b
Yelihi <Yelihi19@gmail.com>
2023-11-02 13:36:18 +0900

    Docs: TIL beakjoon 숨바꼭질2

--- javascript/Algorithm/백준/Beakjoon/BFS/숨바꼭질2.js
+++ javascript/Algorithm/백준/Beakjoon/BFS/숨바꼭질2.js
@@ -1,31 +1,3 @@
-// 백준에 바꿔서 입력할 버전
-// let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-
-// 하나의 값을 받을 때
-// let input = require("fs").readFileSync("example.txt").toString().trim();
-
-// 한 줄에 공백으로 값이 들어올 때
-// let input = require("fs")
-//   .readFileSync("example.txt")
-//   .toString()
-//   .trim()
-//   .split("\n");
-
-// 한줄에 하나씩 값이 들어올 때
-// let input = require("fs").readFileSync("example.txt").toString().trim().split("\n");
-
-// 첫 번째 줄에 자연수 n 을 받고, 그 다음 줄에 공백으로 구분된 n개의 값들을 입력받을 때
-// let input = require("fs").readFileSync("example.txt").toString().trim().split(/\s+/);
-// const [n, ...arr] = input;
-
-// 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 n개의 줄에 걸쳐 한 줄에 하나의 값을 입력받을 때
-// let input = require("fs").readFileSync("example.txt").toString().trim().split('\n');
-// const [n, ...arr] = input;
-
-// 디 버그 할때 : node index
-
-// 급할때 사용하는 버전
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -90,7 +62,7 @@ const [start, end] = input.split(" ").map((v) => Number(v));
 let times = 0;
 let tools = 0;
 
-const visited = new Array(200001).fill(0);
+const visited = new Array(100001).fill(0);
 
 function bfs(start) {
   visited[start] = 1;
