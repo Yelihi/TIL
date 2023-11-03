34edd9688736ee3e784dbea97e3682e7b775b309
Yelihi <Yelihi19@gmail.com>
2023-11-03 12:39:58 +0900

    Docs: TIL beakjoon 01타일

---
+++ javascript/Algorithm/백준/Beakjoon/DP/1904 - 01타일.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(__dirname + "/example.txt")
+  .toString()
+  .trim();
+
+const N = Number(input);
+
+const dp = [];
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+}
+
+console.log(dp[N]);
