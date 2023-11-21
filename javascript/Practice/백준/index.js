9b91cb0163662208d96c61b421f1bb6fe6913709
Yelihi <Yelihi19@gmail.com>
2023-11-21 15:11:01 +0900

    test

--- javascript/Practice/백준/index.js
+++ javascript/Practice/백준/index.js
@@ -30,35 +30,77 @@ const fs = require("fs");
 const input = fs
   .readFileSync(__dirname + "/index.txt")
   .toString()
-  .trim()
-  .split(/\n/);
+  .trim();
 
-const iterator = input[Symbol.iterator]();
-const N = Number(iterator.next().value);
+const N = Number(input);
 
-for (let i = 0; i < N; i++) {
-  const row = Number(iterator.next().value);
+const dp = [];
+const answer = [];
+answer[0] = N;
 
-  const top = iterator
-    .next()
-    .value.split(" ")
-    .map((v) => Number(v));
-  const bottom = iterator
-    .next()
-    .value.split(" ")
-    .map((v) => Number(v));
+dp[0] = 0;
+dp[1] = 0;
+dp[2] = 1;
+dp[3] = 1;
 
-  const dp = Array.from(Array(3), () => []);
-
-  dp[0][0] = top[0];
-  dp[1][0] = bottom[0];
-  dp[2][0] = 0;
-
-  for (let j = 1; j < row; j++) {
-    dp[0][j] = Math.max(dp[1][j - 1], dp[2][j - 1]) + top[j];
-    dp[1][j] = Math.max(dp[0][j - 1], dp[2][j - 1]) + bottom[j];
-    dp[2][j] = Math.max(dp[0][j - 1], dp[1][j - 1]);
+for (let i = 4; i <= N; i++) {
+  if (i % 3 == 0 && i % 2 == 0) {
+    dp[i] = Math.min(dp[i / 3], dp[i / 2], dp[i - 1]) + 1;
+  } else if (i % 3 == 0) {
+    dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
+  } else if (i % 2 == 0) {
+    dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+  } else {
+    dp[i] = dp[i - 1] + 1;
   }
-
-  console.log(Math.max(dp[0][row - 1], dp[1][row - 1], dp[2][row - 1]));
 }
+
+let backTrack = N;
+
+while (backTrack !== 1) {
+  const map = new Map();
+  if (backTrack % 3 == 0 && backTrack % 2 == 0) {
+    map.set(backTrack / 3, dp[backTrack / 3]);
+    map.set(backTrack / 2, dp[backTrack / 2]);
+    map.set(backTrack - 1, dp[backTrack - 1]);
+    const min = Math.min(
+      map.get(backTrack / 3),
+      map.get(backTrack / 2),
+      map.get(backTrack - 1)
+    );
+    for (const [key, value] of map.entries()) {
+      if (value === min) {
+        answer.push(key);
+        backTrack = key;
+        break;
+      }
+    }
+  } else if (backTrack % 3 == 0) {
+    map.set(backTrack / 3, dp[backTrack / 3]);
+    map.set(backTrack - 1, dp[backTrack - 1]);
+    const min = Math.min(map.get(backTrack / 3), map.get(backTrack - 1));
+    for (const [key, value] of map.entries()) {
+      if (value === min) {
+        answer.push(key);
+        backTrack = key;
+        break;
+      }
+    }
+  } else if (backTrack % 2 == 0) {
+    map.set(backTrack / 2, dp[backTrack / 2]);
+    map.set(backTrack - 1, dp[backTrack - 1]);
+    const min = Math.min(map.get(backTrack / 2), map.get(backTrack - 1));
+    for (const [key, value] of map.entries()) {
+      if (value === min) {
+        answer.push(key);
+        backTrack = key;
+        break;
+      }
+    }
+  } else {
+    answer.push(backTrack - 1);
+    backTrack = backTrack - 1;
+  }
+}
+
+console.log(answer);
