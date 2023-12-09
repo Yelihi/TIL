c9d2975c8f771063d3f654668eff32eb6829753c
Yelihi <Yelihi19@gmail.com>
2023-12-09 23:55:51 +0900

    Docs: TIL leetcode

--- javascript/Practice/LeetCode/easy/605. Can Place Flowers.js
+++ javascript/Practice/LeetCode/easy/605. Can Place Flowers.js
@@ -36,3 +36,14 @@ var canPlaceFlowers = function (flowerbed, n) {
 
   return n > 0 ? false : true;
 };
+
+// 다른사람 풀이
+
+function canPlaceFlowers(f, n) {
+  if (!n) return true;
+  for (let i = 0; i < f.length; i += 2)
+    if (!f[i])
+      if (f[i + 1]) i++;
+      else if (!--n) return true;
+  return false;
+}
